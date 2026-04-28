153d910a70a3949132a7e5bff9f553e60c2e9e2a
Slava <slavik123121@icloud.com>
2026-04-28 23:03:27 +0300

    complete feedback widget (react + typescript)

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 import 'modern-normalize';
 import './global.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
